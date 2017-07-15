568128930efe3781c359534b33b84641c7b3be56
Richard Middleton <richard@richardmiddleton.me>
2017-07-15 16:52:05 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect('mongodb://localhost/yelp_camp');
+// mongoose.connect('mongodb://localhost/yelp_camp');
+mongoose.connect('mongodb://Middi:youandme123@ds161012.mlab.com:61012/yelpcamp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
